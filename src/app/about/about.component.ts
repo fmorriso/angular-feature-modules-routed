e6aee635bff39efe759c90131974fe149ed97441
Fred Morrison <fpmorrison@verizon.net>
2019-02-17 14:45:07 -0500

    added Angular version number to About page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, VERSION} from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+	selector: 'app-about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+	angularVersion: string;
 
-  constructor() { }
+	constructor() {
+		this.angularVersion = VERSION.full;
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
